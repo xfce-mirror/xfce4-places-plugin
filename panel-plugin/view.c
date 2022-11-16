xfce-mirror/xfce4-places-plugin

commit 1c1cb21a89ae2a569ece5a8726977cfa6fb7f4d9

Fix compilation warnings

panel-plugin/view.c
@@ -447,8 +447,7 @@ pview_cb_recent_items_clear(GtkWidget *clear_item, GtkWidget *recent_menu)
 {
     GtkRecentManager *manager = gtk_recent_manager_get_default();
 
-    gint removed = gtk_recent_manager_purge_items(manager, NULL);
-    DBG("Cleared %d recent items", removed);
+    DBG("Cleared %d recent items", gtk_recent_manager_purge_items(manager, NULL));
 
     pview_cb_recent_changed(manager, recent_menu);
 

commit 7ff6551f5ceef93dca53f96ffb22ba0ba0ff32c8

Fix function called only in debug mode
Regression introduced in 1c1cb21a89ae2a569ece5a8726977cfa6fb7f4d9.

Related: !6, 1c1cb21a89ae2a569ece5a8726977cfa6fb7f4d9#note_59970
MR: !11

panel-plugin/view.c
@@ -445,8 +445,11 @@ static gboolean
 pview_cb_recent_items_clear(GtkWidget *clear_item, GtkWidget *recent_menu)
 {
     GtkRecentManager *manager = gtk_recent_manager_get_default();
-
-    DBG("Cleared %d recent items", gtk_recent_manager_purge_items(manager, NULL));
+#if defined(DEBUG) && DEBUG > 0
+    gint removed =
+#endif
+    gtk_recent_manager_purge_items(manager, NULL);
+    DBG("Cleared %d recent items", removed);
 
     pview_cb_recent_changed(manager, recent_menu);
 