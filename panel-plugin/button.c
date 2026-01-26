xfce-mirror/xfce4-places-plugin

commit 7337282f0962914816b3032fa16f25f9e47e271c

Do not play with plugin reference count
This breaks panel itembar internal logic when plugin is run as internal,
as it unrefs plugins in a loop.

panel-plugin/button.c
@@ -236,8 +236,8 @@ places_button_construct(PlacesButton *self, XfcePanelPlugin *plugin)
 
     g_assert(XFCE_IS_PANEL_PLUGIN(plugin));
 
-    g_object_ref(plugin);
     self->plugin = plugin;
+    g_object_add_weak_pointer(G_OBJECT(plugin), (gpointer *)&self->plugin);
 
     /* from xfce_panel_create_toggle_button() (libxfce4panel) */
     gtk_widget_set_can_default (GTK_WIDGET(self), FALSE);
@@ -302,11 +302,6 @@ places_button_dispose(GObject *object)
     if (self->screen_changed_id != 0) {
         g_signal_handler_disconnect(self, self->screen_changed_id);
         self->screen_changed_id = 0;
-    }
-
-    if (self->plugin != NULL) {
-        g_object_unref(self->plugin);
-        self->plugin = NULL;
     }
 
     (*G_OBJECT_CLASS(places_button_parent_class)->dispose) (object);

commit 17210856d9972517ba10fed9ba63ca568dc87173

Don't reset plugin pointer when disposed
The parent structure is already freed at that time.

Fixes: 7337282f0962914816b3032fa16f25f9e47e271c

panel-plugin/button.c
@@ -237,7 +237,6 @@ places_button_construct(PlacesButton *self, XfcePanelPlugin *plugin)
     g_assert(XFCE_IS_PANEL_PLUGIN(plugin));
 
     self->plugin = plugin;
-    g_object_add_weak_pointer(G_OBJECT(plugin), (gpointer *)&self->plugin);
 
     /* from xfce_panel_create_toggle_button() (libxfce4panel) */
     gtk_widget_set_can_default (GTK_WIDGET(self), FALSE);